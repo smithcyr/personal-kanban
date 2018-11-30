380eaa0478d92f32e7afbab579d8f91ba7ffbe52
Cyrus Smith <cyrus@sapho.com>
2018-11-30 12:54:22 +0100

    remove task section height

--- PersonalKanban/src/TaskList.js
+++ PersonalKanban/src/TaskList.js
@@ -5,7 +5,7 @@ import gql from 'graphql-tag';
 
 const styles = {
   outer: { paddingTop: 32, paddingLeft: 10, paddingRight: 10 },
-  wrapper: { height: 40, marginBottom: 15, flex: 1, flexDirection: 'row' },
+  wrapper: { marginBottom: 15, flex: 1, flexDirection: 'row' },
   header: { fontSize: 20 },
   subtextWrapper: { flex: 1, flexDirection: 'row' },
   votes: { color: '#999' },
@@ -19,6 +19,7 @@ function TaskList({ data: { loading, tasks } }) {
   } else {
     return (
       <View style={styles.outer}>
+        <Text>Tasks</Text>
         {(tasks || []).sort((x, y) => y.id - x.id).map(task => (
           <View key={task.id} style={styles.wrapper}>
             <View>
@@ -39,11 +40,13 @@ function TaskList({ data: { loading, tasks } }) {
 // The `graphql` wrapper executes a GraphQL query and makes the results
 // available on the `data` prop of the wrapped component (TaskList here)
 export default graphql(gql`
-  query tasks {
+query {
+  tasks {
     id
     name
     content
     state
     priority
   }
+}
 `)(TaskList);
